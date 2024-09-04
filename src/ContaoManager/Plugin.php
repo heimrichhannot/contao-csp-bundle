8e0e0bdb37b8415c7f4449de23722ce193b07341
Thomas Körner <t.koerner@heimrich-hannot.de>
2024-09-04 11:37:09 +0200

    register services and routes

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,9 +6,14 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use HeimrichHannot\CspBundle\HeimrichHannotCspBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-class Plugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface, ConfigPluginInterface, RoutingPluginInterface
 {
 
     /**
@@ -20,5 +25,15 @@ class Plugin implements BundlePluginInterface
             BundleConfig::create(HeimrichHannotCspBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class])
         ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig)
+    {
+        $loader->load('@HeimrichHannotCspBundle/config/services.yaml');
+    }
+
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    {
+        return $resolver->resolve('@HeimrichHannotCspBundle/config/routing.yaml')->load('@HeimrichHannotCspBundle/config/routing.yaml');
     }
 }
